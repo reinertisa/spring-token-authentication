8dd4f4a61ab385e1c164865ee0e5cdd216ca1d76
Isa Reinert <reinert.isa@gmail.com>
2025-03-04 01:20:26 -0800

    feat(api): initialize project

---
+++ sta-api/src/main/java/com/reinertisa/sta/StaApiApplication.java
@@ -0,0 +1,13 @@
+package com.reinertisa.sta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StaApiApplication.class, args);
+    }
+
+}
