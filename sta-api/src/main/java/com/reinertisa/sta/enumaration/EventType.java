3c2eb19f00c46846e3b3bbf40fc710510054c20a
Isa Reinert <reinert.isa@gmail.com>
2025-03-05 01:33:16 -0800

    feat(api): create email service

---
+++ sta-api/src/main/java/com/reinertisa/sta/enumaration/EventType.java
@@ -0,0 +1,6 @@
+package com.reinertisa.sta.enumaration;
+
+public enum EventType {
+    REGISTRATION,
+    RESET_PASSWORD,
+}
---
+++ sta-api/src/main/java/com/reinertisa/sta/service/EmailService.java
@@ -0,0 +1,8 @@
+package com.reinertisa.sta.service;
+
+public interface EmailService {
+
+    void setNewAccountEmail(String name, String email, String token);
+
+    void sendPasswordResetEmail(String name, String email, String token);
+}
