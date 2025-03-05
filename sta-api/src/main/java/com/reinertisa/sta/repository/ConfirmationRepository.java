ec2d4ec90952caa3d73825c76cdd285cb70c889a
Isa Reinert <reinert.isa@gmail.com>
2025-03-05 01:48:53 -0800

    feat(api): create entity repositories

---
+++ sta-api/src/main/java/com/reinertisa/sta/repository/ConfirmationRepository.java
@@ -0,0 +1,14 @@
+package com.reinertisa.sta.repository;
+
+import com.reinertisa.sta.entity.ConfirmationEntity;
+import com.reinertisa.sta.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ConfirmationRepository extends JpaRepository<ConfirmationEntity, Long> {
+    Optional<ConfirmationEntity> findByKey(String key);
+    Optional<ConfirmationEntity> findByUserEntity(UserEntity userEntity);
+}
---
+++ sta-api/src/main/java/com/reinertisa/sta/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.reinertisa.sta.repository;
+
+import com.reinertisa.sta.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByEmailIgnoreCase(String email);
+    Optional<UserEntity> findUserByUserId(String userId);
+}
