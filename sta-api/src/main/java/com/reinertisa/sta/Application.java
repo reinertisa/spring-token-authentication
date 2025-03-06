5f7fe617a741a5cbb4cff3834958b4e6602f3503
Isa Reinert <reinert.isa@gmail.com>
2025-03-06 01:25:37 -0800

    feat(api): enable jpa auditing and async in application

--- sta-api/src/main/java/com/reinertisa/sta/Application.java
+++ sta-api/src/main/java/com/reinertisa/sta/Application.java
@@ -1,13 +1,41 @@
 package com.reinertisa.sta;
 
+import com.reinertisa.sta.domain.RequestContext;
+import com.reinertisa.sta.entity.RoleEntity;
+import com.reinertisa.sta.enumaration.Authority;
+import com.reinertisa.sta.repository.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableAsync
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(RoleRepository roleRepository)
+    {
+        return args -> {
+            RequestContext.setUserId(0L);
+            var userRole = new RoleEntity();
+            userRole.setName(Authority.USER.name());
+            userRole.setAuthorities(Authority.USER);
+            roleRepository.save(userRole);
+
+            var adminRole = new RoleEntity();
+            adminRole.setName(Authority.ADMIN.name());
+            adminRole.setAuthorities(Authority.ADMIN);
+            roleRepository.save(adminRole);
+            RequestContext.start();
+        };
     }
 
 }
