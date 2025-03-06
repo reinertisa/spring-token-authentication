2f174b5b6e86f1f714295c4d487fd228d440f27a
Isa Reinert <reinert.isa@gmail.com>
2025-03-06 02:04:46 -0800

    fix(api): update email utils

--- sta-api/src/main/java/com/reinertisa/sta/utils/EmailUtils.java
+++ sta-api/src/main/java/com/reinertisa/sta/utils/EmailUtils.java
@@ -2,21 +2,21 @@ package com.reinertisa.sta.utils;
 
 public class EmailUtils {
 
-    public static String getEmailMessage(String name, String host, String token) {
+    public static String getEmailMessage(String name, String host, String key) {
         return "Hello " + name + "\n\nYour new account has been created. Please click on the link below to " +
-                "verify your account.\n\n" + getVerificationUrl(host, token) + "\n\nThe Support team";
+                "verify your account.\n\n" + getVerificationUrl(host, key) + "\n\nThe Support team";
     }
 
-    public static String getResetPasswordMessage(String name, String host, String token) {
+    public static String getResetPasswordMessage(String name, String host, String key) {
         return "Hello " + name + "\n\nYour new account has been created. Please click on the link below to " +
-                "verify your account.\n\n" + getResetPasswordUrl(host, token) + "\n\nThe Support team";
+                "verify your account.\n\n" + getResetPasswordUrl(host, key) + "\n\nThe Support team";
     }
 
-    private static String getResetPasswordUrl(String host, String token) {
-        return host + "/verify/password?token=" + token;
+    private static String getResetPasswordUrl(String host, String key) {
+        return host + "/verify/password?key=" + key;
     }
 
-    private static String getVerificationUrl(String host, String token) {
-        return host + "/verify/account?token=" + token;
+    private static String getVerificationUrl(String host, String key) {
+        return host + "/verify/account?key=" + key;
     }
 }
