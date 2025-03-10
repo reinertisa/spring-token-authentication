d76256ef0ae1fb757db6ce8f7d7eb85f47e067c2
Isa Reinert <reinert.isa@gmail.com>
2025-03-09 17:13:46 -0700

    feat(api): update constants

--- sta-api/src/main/java/com/reinertisa/sta/constant/Constants.java
+++ sta-api/src/main/java/com/reinertisa/sta/constant/Constants.java
@@ -1,6 +1,14 @@
 package com.reinertisa.sta.constant;
 
 public class Constants {
+    public static final String LOGIN_PATH = "/user/login";
+    public static final int NINETY_DAYS = 90;
+    public static final int STRENGTH = 12;
+    public static final String AUTHORITIES = "authorities";
+    public static final String GET_ARRAYS_LLC = "GET_ARRAYS_LLC";
+    public static final String EMPTY_VALUE = "empty";
+
+    public static final String ROLE = "role";
     public static final String ROLE_PREFIX = "ROLE_";
     public static final String AUTHORITY_DELIMITER = ",";
     public static final String USER_AUTHORITIES = "document:create,document:read,document:update,document:delete";
