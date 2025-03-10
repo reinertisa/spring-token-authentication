c3d8f6ab026b0c8a734bf3db3cc20367b8bbaf00
Isa Reinert <reinert.isa@gmail.com>
2025-03-09 17:17:16 -0700

    feat(api): update user utils

--- sta-api/src/main/java/com/reinertisa/sta/utils/UserUtils.java
+++ sta-api/src/main/java/com/reinertisa/sta/utils/UserUtils.java
@@ -1,12 +1,16 @@
 package com.reinertisa.sta.utils;
 
+import com.reinertisa.sta.dto.User;
+import com.reinertisa.sta.entity.CredentialEntity;
 import com.reinertisa.sta.entity.RoleEntity;
 import com.reinertisa.sta.entity.UserEntity;
+import org.springframework.beans.BeanUtils;
 
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import static com.reinertisa.sta.constant.Constants.NINETY_DAYS;
 import static org.apache.commons.lang3.StringUtils.EMPTY;
 
 public class UserUtils {
@@ -28,5 +32,21 @@ public class UserUtils {
                .imageUrl("https://cdn-icon-png.flaticon/512/149/149071.png")
                .role(role)
                .build();
+    }
+
+    public static User fromUserEntity(UserEntity userEntity, RoleEntity role, CredentialEntity credentialEntity) {
+        User user = new User();
+        BeanUtils.copyProperties(userEntity, user);
+        user.setLastLogin(userEntity.getLastLogin().toString());
+        user.setCredentialsNonExpired(isCredentialsNonExpired(credentialEntity));
+        user.setCreatedAt(userEntity.getCreatedAt().toString());
+        user.setUpdatedAt(userEntity.getUpdatedAt().toString());
+        user.setRole(role.getName());
+        user.setAuthorities(role.getAuthorities().getValue());
+        return user;
+    }
+
+    private static boolean isCredentialsNonExpired(CredentialEntity credentialEntity) {
+        return credentialEntity.getUpdatedAt().plusDays(NINETY_DAYS).isAfter(LocalDateTime.now());
     }
 }
