bab047b3ede965138d6d646045a4ef434581ff17
Isa Reinert <reinert.isa@gmail.com>
2025-03-09 17:16:32 -0700

    feat(api): update user resource

--- sta-api/src/main/java/com/reinertisa/sta/resource/UserResource.java
+++ sta-api/src/main/java/com/reinertisa/sta/resource/UserResource.java
@@ -7,13 +7,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
-import java.util.Map;
 
 import static com.reinertisa.sta.utils.RequestUtils.getResponse;
 import static java.util.Collections.emptyMap;
@@ -26,7 +22,6 @@ import static org.springframework.http.HttpStatus.OK;
 public class UserResource {
 
     private final UserService userService;
-    private final AuthenticationManager authenticationManager;
 
     @PostMapping("/register")
     public ResponseEntity<Response> saveUser(@RequestBody @Valid UserRequest user, HttpServletRequest request) {
@@ -40,14 +35,6 @@ public class UserResource {
     public ResponseEntity<Response> verifyAccount(@RequestParam("key") String key, HttpServletRequest request) {
         userService.verifyAccountKey(key);
         return ResponseEntity.ok().body(getResponse(request, emptyMap(), "Account verified.", OK));
-    }
-
-    @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody UserRequest user) {
-        UsernamePasswordAuthenticationToken unauthenticated = UsernamePasswordAuthenticationToken.unauthenticated(
-                user.getEmail(), user.getPassword());
-        Authentication authenticate = authenticationManager.authenticate(unauthenticated);
-        return ResponseEntity.ok().body(Map.of("user", authenticate));
     }
 
     private URI getUri() {
