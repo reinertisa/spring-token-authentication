fa951f9eafd48b648b78cfad160809e0c287b099
Isa Reinert <reinert.isa@gmail.com>
2025-03-09 17:13:27 -0700

    feat(api): create authentication filter

---
+++ sta-api/src/main/java/com/reinertisa/sta/security/AuthenticationFilter.java
@@ -0,0 +1,86 @@
+package com.reinertisa.sta.security;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.reinertisa.sta.domain.ApiAuthentication;
+import com.reinertisa.sta.domain.Response;
+import com.reinertisa.sta.dto.User;
+import com.reinertisa.sta.dtorequest.LoginRequest;
+import com.reinertisa.sta.enumaration.LoginType;
+import com.reinertisa.sta.enumaration.TokenType;
+import com.reinertisa.sta.service.JwtService;
+import com.reinertisa.sta.service.UserService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.io.IOException;
+import java.util.Map;
+
+import static com.reinertisa.sta.constant.Constants.LOGIN_PATH;
+import static com.reinertisa.sta.utils.RequestUtils.getResponse;
+import static com.reinertisa.sta.utils.RequestUtils.handleErrorResponse;
+import static org.springframework.http.HttpMethod.POST;
+
+@Slf4j
+public class AuthenticationFilter extends AbstractAuthenticationProcessingFilter {
+    private final UserService userService;
+    private final JwtService jwtService;
+
+    public AuthenticationFilter(AuthenticationManager authenticationManager,
+                                UserService userService, JwtService jwtService) {
+        super(new AntPathRequestMatcher(LOGIN_PATH, POST.name()), authenticationManager);
+        this.userService = userService;
+        this.jwtService = jwtService;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+            throws AuthenticationException, IOException, ServletException {
+        try {
+            var user = new ObjectMapper()
+                    .configure(JsonParser.Feature.AUTO_CLOSE_SOURCE, true)
+                    .readValue(request.getInputStream(), LoginRequest.class);
+            userService.updateLoginAttempt(user.getEmail(), LoginType.LOGIN_ATTEMPT);
+            var authentication = ApiAuthentication.unauthenticated(user.getEmail(), user.getPassword());
+            return getAuthenticationManager().authenticate(authentication);
+        } catch (Exception ex) {
+            log.error(ex.getMessage());
+            handleErrorResponse(request, response, ex);
+            return null;
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+                                            Authentication authentication) throws IOException, ServletException {
+        var user = (User) authentication.getPrincipal();
+        userService.updateLoginAttempt(user.getEmail(), LoginType.LOGIN_SUCCESS);
+        var httpResponse = user.isMfa() ? sendQrCode(request, user) : sendResponse(request, response, user);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpStatus.OK.value());
+        var out = response.getOutputStream();
+        var mapper = new ObjectMapper();
+        mapper.writeValue(out, httpResponse);
+        out.flush();
+    }
+
+    private Response sendResponse(HttpServletRequest request, HttpServletResponse response, User user) {
+        jwtService.addCookie(response, user, TokenType.ACCESS);
+        jwtService.addCookie(response, user, TokenType.REFRESH);
+        return getResponse(request, Map.of("user", user), "Login Success", HttpStatus.OK);
+    }
+
+    private Response sendQrCode(HttpServletRequest request, User user) {
+        return getResponse(request, Map.of("user", user), "Please enter QR Code", HttpStatus.OK);
+    }
+}
