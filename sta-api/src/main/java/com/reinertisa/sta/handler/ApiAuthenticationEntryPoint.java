ae181a9a222c15c240234d68a8c9b59dc801acdb
Isa Reinert <reinert.isa@gmail.com>
2025-03-10 00:28:20 -0700

    feat(api): create handler classes

---
+++ sta-api/src/main/java/com/reinertisa/sta/handler/ApiAuthenticationEntryPoint.java
@@ -0,0 +1,21 @@
+package com.reinertisa.sta.handler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+import static com.reinertisa.sta.utils.RequestUtils.handleErrorResponse;
+
+@Component
+public class ApiAuthenticationEntryPoint implements AuthenticationEntryPoint
+{
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        handleErrorResponse(request, response, exception);
+    }
+}
