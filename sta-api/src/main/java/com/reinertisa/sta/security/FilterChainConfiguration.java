f9f071987761e60aa719ff9d25d8d6d31f252581
Isa Reinert <reinert.isa@gmail.com>
2025-03-10 00:30:57 -0700

    feat(api): update filter chain configuration

--- sta-api/src/main/java/com/reinertisa/sta/security/FilterChainConfiguration.java
+++ sta-api/src/main/java/com/reinertisa/sta/security/FilterChainConfiguration.java
@@ -1,63 +1,70 @@
 package com.reinertisa.sta.security;
 
+import com.reinertisa.sta.handler.ApiAccessDeniedHandler;
+import com.reinertisa.sta.handler.ApiAuthenticationEntryPoint;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.ProviderManager;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static com.google.common.net.HttpHeaders.X_REQUESTED_WITH;
+import static com.reinertisa.sta.constant.Constants.*;
+import static org.springframework.http.HttpHeaders.*;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
+@RequiredArgsConstructor
 public class FilterChainConfiguration {
+    private final ApiAccessDeniedHandler apiAccessDeniedHandler;
+    private final ApiAuthenticationEntryPoint apiAuthenticationEntryPoint;
+    private final ApiHttpConfigurer apiHttpConfigurer;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
+        http
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(corsConfigurer -> corsConfigurer.configurationSource(corsConfigurationSource()))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .exceptionHandling(exception ->
+                        exception.accessDeniedHandler(apiAccessDeniedHandler)
+                                .authenticationEntryPoint(apiAuthenticationEntryPoint))
                 .authorizeHttpRequests(request ->
-                        request.requestMatchers("/user/login")
-                                .permitAll()
-                                .anyRequest()
-                                .authenticated())
-                .build();
+                        request.requestMatchers(PUBLIC_URLS).permitAll()
+                                .requestMatchers(HttpMethod.OPTIONS).permitAll()
+                                .requestMatchers(HttpMethod.DELETE, "/user/delete/**")
+                                .hasAuthority("user:delete")
+                                .requestMatchers(HttpMethod.DELETE, "/document/delete/**")
+                                .hasAuthority("document:delete")
+                                .anyRequest().authenticated())
+                .with(apiHttpConfigurer, Customizer.withDefaults());
+        return http.build();
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(UserDetailsService userDetailsService) {
-        ApiAuthenticationProvider myOwnAuthenticationProvider = new ApiAuthenticationProvider(userDetailsService);
-        return new ProviderManager(myOwnAuthenticationProvider);
-    }
-
-//    @Bean
-//    public UserDetailsService userDetailsService() {
-//        var user1 = User.withDefaultPasswordEncoder()
-//                .username("bilirkishi")
-//                .password("{noop}letmein")
-//                .roles("USER")
-//                .build();
-//
-//        var user2 = User.withDefaultPasswordEncoder()
-//                .username("bulgurcu")
-//                .password("{noop}letmein")
-//                .roles("USER")
-//                .build();
-//
-//        return new InMemoryUserDetailsManager(List.of(user1, user2));
-//    }
-
-    @Bean
-    public UserDetailsService inMemoryUserDetailsManager() {
-        return new InMemoryUserDetailsManager(
-                User.withUsername("bilirkishi").password("letmein").roles("USER").build(),
-                User.withUsername("bulgurcu").password("letmein").roles("USER").build()
-        );
+    public CorsConfigurationSource corsConfigurationSource() {
+        var corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setAllowedOrigins(List.of("http://securedoc.com", "http://192.168.1.159:3000"));
+        corsConfiguration.setAllowedHeaders(Arrays.asList(ORIGIN, ACCESS_CONTROL_ALLOW_ORIGIN, CONTENT_TYPE, ACCEPT, AUTHORIZATION, X_REQUESTED_WITH, ACCESS_CONTROL_REQUEST_METHOD, ACCESS_CONTROL_REQUEST_HEADERS, ACCESS_CONTROL_ALLOW_CREDENTIALS, FILE_NAME));
+        corsConfiguration.setExposedHeaders(Arrays.asList(ORIGIN, ACCESS_CONTROL_ALLOW_ORIGIN, CONTENT_TYPE, ACCEPT, AUTHORIZATION, X_REQUESTED_WITH, ACCESS_CONTROL_REQUEST_METHOD, ACCESS_CONTROL_REQUEST_HEADERS, ACCESS_CONTROL_ALLOW_CREDENTIALS, FILE_NAME));
+        corsConfiguration.setAllowedMethods(Arrays.asList(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PUT.name(), HttpMethod.DELETE.name(), HttpMethod.OPTIONS.name()));
+        corsConfiguration.setMaxAge(3600L);
+        var source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration(BASE_PATH, corsConfiguration);
+        return source;
     }
 }
