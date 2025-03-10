eb0189e6b03ca6a8a3a79df778ca6bac58838dcb
Isa Reinert <reinert.isa@gmail.com>
2025-03-09 17:12:30 -0700

    feat(api): create api authentication for security

---
+++ sta-api/src/main/java/com/reinertisa/sta/domain/ApiAuthentication.java
@@ -0,0 +1,70 @@
+package com.reinertisa.sta.domain;
+
+import com.reinertisa.sta.dto.User;
+import com.reinertisa.sta.exception.ApiException;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+
+import java.util.Collection;
+
+public class ApiAuthentication extends AbstractAuthenticationToken {
+
+    private static final String PASSWORD_PROTECTED = "[PASSWORD_PROTECTED]";
+    private static final String EMAIL_PROTECTED = "[EMAIL_PROTECTED]";
+    private User user;
+    private String email;
+    private String password;
+    private boolean authenticated;
+
+    private ApiAuthentication(String email, String password) {
+        super(AuthorityUtils.NO_AUTHORITIES);
+        this.email = email;
+        this.password = password;
+        this.authenticated = false;
+    }
+
+    private ApiAuthentication(User user, Collection<? extends GrantedAuthority> authorities) {
+        super(authorities);
+        this.user = user;
+        this.password = PASSWORD_PROTECTED;
+        this.email = EMAIL_PROTECTED;
+        this.authenticated = true;
+    }
+
+    public static ApiAuthentication unauthenticated(String email, String password) {
+        return new ApiAuthentication(email, password);
+    }
+
+    public static ApiAuthentication authenticated(User user, Collection<? extends GrantedAuthority> authorities) {
+        return new ApiAuthentication(user, authorities);
+    }
+
+    @Override
+    public Object getCredentials() {
+        return PASSWORD_PROTECTED;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return this.user;
+    }
+
+    @Override
+    public void setAuthenticated(boolean authenticated) {
+        throw new ApiException("You cannot set authentication");
+    }
+
+    @Override
+    public boolean isAuthenticated() {
+        return this.authenticated;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+}
