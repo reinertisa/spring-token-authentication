981da1881ec90c55df23fa0dc46af63f655e10c6
Isa Reinert <reinert.isa@gmail.com>
2025-03-12 23:11:25 -0700

    feat(api): add qr code utils

--- sta-api/src/main/java/com/reinertisa/sta/utils/UserUtils.java
+++ sta-api/src/main/java/com/reinertisa/sta/utils/UserUtils.java
@@ -4,13 +4,22 @@ import com.reinertisa.sta.dto.User;
 import com.reinertisa.sta.entity.CredentialEntity;
 import com.reinertisa.sta.entity.RoleEntity;
 import com.reinertisa.sta.entity.UserEntity;
+import com.reinertisa.sta.exception.ApiException;
+import dev.samstevens.totp.code.HashingAlgorithm;
+import dev.samstevens.totp.qr.QrData;
+import dev.samstevens.totp.qr.ZxingPngQrGenerator;
+import dev.samstevens.totp.secret.DefaultSecretGenerator;
 import org.springframework.beans.BeanUtils;
 
 
 import java.time.LocalDateTime;
 import java.util.UUID;
+import java.util.function.BiFunction;
+import java.util.function.Supplier;
 
+import static com.reinertisa.sta.constant.Constants.GET_ARRAYS_LLC;
 import static com.reinertisa.sta.constant.Constants.NINETY_DAYS;
+import static dev.samstevens.totp.util.Utils.getDataUriForImage;
 import static org.apache.commons.lang3.StringUtils.EMPTY;
 
 public class UserUtils {
@@ -49,4 +58,29 @@ public class UserUtils {
     private static boolean isCredentialsNonExpired(CredentialEntity credentialEntity) {
         return credentialEntity.getUpdatedAt().plusDays(NINETY_DAYS).isAfter(LocalDateTime.now());
     }
+
+    public static BiFunction<String, String, QrData> qrDataFunction = (email, qrCodeSecret) ->
+            new QrData.Builder()
+                    .issuer(GET_ARRAYS_LLC)
+                    .label(email)
+                    .secret(qrCodeSecret)
+                    .algorithm(HashingAlgorithm.SHA1)
+                    .digits(8)
+                    .period(30)
+                    .build();
+
+    public static BiFunction<String, String, String> qrCodeImageUri = (email, qrCodeSecret) -> {
+        QrData data = qrDataFunction.apply(email, qrCodeSecret);
+        ZxingPngQrGenerator generator = new ZxingPngQrGenerator();
+        byte[] imageData;
+        try {
+            imageData = generator.generate(data);
+        } catch (Exception exception) {
+            throw new ApiException("Unable to create QR code URI");
+        }
+
+        return getDataUriForImage(imageData, generator.getImageMimeType());
+    };
+
+    public static Supplier<String> qrCodeSecret = () -> new DefaultSecretGenerator().generate();
 }
