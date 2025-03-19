30bb5bf5a96db4ea0be80296490efae584624001
Isa Reinert <reinert.isa@gmail.com>
2025-03-19 00:47:16 -0700

    feat(api): create DocumentService

---
+++ sta-api/src/main/java/com/reinertisa/sta/service/DocumentService.java
@@ -0,0 +1,19 @@
+package com.reinertisa.sta.service;
+
+import com.reinertisa.sta.dto.api.IDocument;
+import org.springframework.core.io.Resource;
+import org.springframework.data.domain.Page;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Collection;
+import java.util.List;
+
+public interface DocumentService {
+    Page<IDocument> getDocuments(int page, int size);
+    Page<IDocument> getDocuments(int page, int size, String name);
+    Collection<IDocument> saveDocuments(String userId, List<MultipartFile> documents);
+    IDocument updateDocument(String documentId, String name, String description);
+    void deleteDocument(String documentId);
+    IDocument getDocumentByDocumentId(String documentId);
+    Resource getResource(String documentName);
+}
