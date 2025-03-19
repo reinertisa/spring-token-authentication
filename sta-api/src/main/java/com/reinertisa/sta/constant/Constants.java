76cc0ee25dde1b5d633cda31e7aed1c9b8cd8ac6
Isa Reinert <reinert.isa@gmail.com>
2025-03-19 02:59:55 -0700

    fix(sql): update sql query

--- sta-api/src/main/java/com/reinertisa/sta/constant/Constants.java
+++ sta-api/src/main/java/com/reinertisa/sta/constant/Constants.java
@@ -23,17 +23,17 @@ public class Constants {
     public static final String MANAGER_AUTHORITIES = "document:create,document:read,document:update,document:delete";
 
     // Query
-    public static final String SELECT_DOCUMENTS_QUERY = "SELECT doc.id, doc.document_id, " +
+    public static final String SELECT_DOCUMENTS_QUERY = "SELECT doc.id, doc.document_id, doc.name, " +
             "doc.description, doc.uri, doc.icon, doc.size, doc.formatted_size, doc.extension, " +
             "doc.reference_id, doc.created_at, doc.updated_at, " +
             "CONCAT(owner.first_name, ' ', owner.last_name) AS owner_name, " +
             "owner.email AS owner_email, " +
             "owner.phone AS owner_phone, " +
-            "owner.last_login AS owner_last_login," +
+            "owner.last_login AS owner_last_login, " +
             "CONCAT(updater.first_name, ' ', updater.last_name) AS updater_name " +
             "FROM documents doc " +
             "JOIN users owner ON owner.id = doc.created_by " +
-            "JOIN users updater ON updater.id = doc.updated_by;";
+            "JOIN users updater ON updater.id = doc.updated_by";
 
     public static final String SELECT_COUNT_DOCUMENTS_QUERY = "SELECT COUNT(*) FROM documents";
 }
