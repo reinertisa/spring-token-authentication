8aa36b3d25e3df11d2203ac0ce8f6ccb8b329e2e
Isa Reinert <reinert.isa@gmail.com>
2025-03-19 13:59:56 -0700

    feat(website): create request utils

---
+++ sta-website/src/utils/requestutils.ts
@@ -0,0 +1,26 @@
+import {IResponse} from "../models/Response.ts";
+
+export const baseUrl = 'http://localhost:8085/user';
+
+export const processResponse = <T>(response: IResponse<T>, meta: any, arg: unknown): IResponse<T> => {
+    const {request} = meta;
+
+    if (request.url.includes('logout')) {
+        localStorage.removeItem("key");
+    }
+    if (!request.url.includes('profile')) {
+        // Show toast notification
+    }
+    console.log({response});
+    return response;
+}
+
+export const processError = (error: {status: number; data: IResponse<void>}, meta: unknown, arg: unknown): {status: number; data: IResponse<void>} => {
+    if (error.data.code === 401 && error.data.status === 'UNAUTHORIZED' && error.data.message === 'You are not logged in') {
+        localStorage.setItem("", "");
+    }
+
+    // Show toast notification
+    console.log({error});
+    return error;
+}
