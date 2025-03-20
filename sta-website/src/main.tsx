581a129926b4f425e1b2df5422ab86c5b47894a8
Isa Reinert <reinert.isa@gmail.com>
2025-03-20 16:29:28 -0700

    feat(website): add login route

--- sta-website/src/main.tsx
+++ sta-website/src/main.tsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import {setupStore} from "./store/store.ts";
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom";
+import Login from "./components/Login.tsx";
+import {Provider} from "react-redux";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const store = setupStore();
+const router = createBrowserRouter(createRoutesFromElements(
+    <Route path="/" element={<App />}>
+        <Route path="login" element={<Login />} />
+    </Route>
+));
+
+const root = createRoot(document.getElementById('root')!);
+
+root.render(
+    <Provider store={store}>
+        <RouterProvider router={router} />
+    </Provider>
 )
