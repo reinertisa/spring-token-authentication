3aeb43ebb95b48bf984185f54ab2d40977f9ed00
Isa Reinert <reinert.isa@gmail.com>
2025-03-21 01:20:11 -0700

    feat(website): create NavBar component

--- sta-website/src/main.tsx
+++ sta-website/src/main.tsx
@@ -2,14 +2,20 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import {setupStore} from "./store/store.ts";
-import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom";
+import {createBrowserRouter, createRoutesFromElements, Navigate, Route, RouterProvider} from "react-router-dom";
 import Login from "./components/Login.tsx";
 import {Provider} from "react-redux";
+import NavBar from "./components/NavBar.tsx";
+import Documents from "./components/document/Documents.tsx";
 
 const store = setupStore();
 const router = createBrowserRouter(createRoutesFromElements(
     <Route path="/" element={<App />}>
         <Route path="login" element={<Login />} />
+        <Route element={<NavBar />}>
+            <Route index path="/documents" element={<Documents />} />
+            <Route path="/" element={<Navigate to={'/documents'} />} />
+        </Route>
     </Route>
 ));
 
