a65851fe7513684214255dd41cbdae03c81cf4fa
Isa Reinert <reinert.isa@gmail.com>
2025-03-24 16:13:16 -0700

    feat(website): add restricted route part - 1

---
+++ sta-website/src/components/Restricted.tsx
@@ -0,0 +1,41 @@
+import {userAPI} from "../services/UserService.ts";
+import {Link, Outlet} from "react-router-dom";
+
+export default function Restricted() {
+
+    const {data: userData, error, isLoading, refetch} = userAPI.useFetchUserQuery(undefined, {refetchOnMountOrArgChange: true});
+
+    if (isLoading || !userData) {
+        return (
+            <div className="container py-5" style={{marginTop: '100px'}}>
+                <div className="row">
+                    <div className="col text-center">
+                        <p>Loading...</p>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    if (userData.data.user.role !== 'USER') {
+        return <Outlet />
+    } else {
+        return (
+            <div className="container py-5">
+                <div className="row">
+                    <div className="col-md-2 text-center">
+                        <p>
+                            <i className="bi bi-exclamation-octagon text-warning" style={{fontSize: '50px'}}></i><br />
+                            Status Code: 403
+                        </p>
+                    </div>
+                    <div className="col-md-10">
+                        <h3>ACCESS DENIED</h3>
+                        <p>Access to this page is denied due to lack of permissions.</p>
+                        <Link to={'/'} className="btn btn-primary">Go Back to Home</Link>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
