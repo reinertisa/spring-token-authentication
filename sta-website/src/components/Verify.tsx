5cd78d05bae79ab4989ad85dfb873b728201b308
Isa Reinert <reinert.isa@gmail.com>
2025-03-24 02:05:25 -0700

    feat(website): verified account part - 3

--- sta-website/src/components/Verify.tsx
+++ sta-website/src/components/Verify.tsx
@@ -1,9 +1,20 @@
 import {Link, useLocation} from "react-router-dom";
+import {userAPI} from "../services/UserService.ts";
+import {useEffect} from "react";
+import {IResponse} from "../models/IResponse.ts";
 
 export default function Verify() {
     const location = useLocation();
     const searchParams = new URLSearchParams(location.search);
     const key = searchParams.get('key');
+
+    const [verifyAccount, {data: accountData, error: accountError, isLoading: accountLoading, isSuccess: accountSuccess}] = userAPI.useVerifyAccountMutation();
+
+    useEffect(() => {
+        if (key && location.pathname.includes('/verify/account')) {
+            verifyAccount(key);
+        }
+    }, [])
 
     if (!key) {
         return (
@@ -36,7 +47,44 @@ export default function Verify() {
         )
     }
 
-    if (location.pathname === '/verify/account') {
+    if (key && !accountSuccess) {
+        return (
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-lg-6 col-md-6 col-sm-12" style={{marginTop: '100px'}}>
+                        <div className="card">
+                            <div className="card-body">
+                                {accountError && <div className="alert alert-dismissible alert-danger">
+                                    {'data' in accountError ? (accountError.data as IResponse<void>).message! : 'An error occurred'}
+                                </div>}
+                                <div className="d-flex align-items-center">
+                                    {!accountError && <><strong role="status">Please wait. Verifying...</strong>}
+                                    <div className="spinner-border ms-auto" aria-hidden="true"></div></>}
+                                </div>
+                                {accountError && <>
+                                    <hr className="my-3" />
+                                    <div className="row mb-3">
+                                        <div className="col d-flex justify-content-start">
+                                            <div className="btn btn-outline-light">
+                                                <Link to="/login" style={{textDecoration: 'none'}}>Go to login</Link>
+                                            </div>
+                                        </div>
+                                        <div className="col d-flex justify-content-end">
+                                            <div className="link-dark">
+                                                <Link to="/resetpassword">Forgot password?</Link>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </>}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+    if (accountSuccess && location.pathname.includes('/verify/account')) {
         return (
             <div className="container">
                 <div className="row justify-content-center">
